b3132ff8dfee8a2d7b8d8578045ca74ddcd95aee
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-02 16:12:08 +0530

    Added: Well come message for employee wage computation program

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.bridgelab.emloyeeWage;
+
+public class EmployeeWageComputation {
+	public static void main(String[] args) {
+		System.out.println("Well come to Employee wage computation program");
+	}
+
+}
