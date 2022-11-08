dd3dc434e49cd1a93a12df3e3c84f04654d3281e
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-08 21:25:18 +0530

    Added:Calculate monthly wage of employee using class and method

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,40 +1,46 @@
 package com.bridgelab.emloyeeWage;
 
-//UC-5 Employee Monthly wage for 20 working days
+//UC-7 Employee Monthly wage for 20 working days and 100 hours using class object and method.
+
 public class EmployeeWageComputation {
 	static int isFullTime = 1;
 	static int isPartTime = 2;
 	static int wagePerHr = 20;
-	static int maxWorkDay =20;
+	static int maxWorkDay = 20;
 	static int maxWorkHr = 100;
-	
+	static int totalWorkingHour = 0;
+
 	public void display() {
 		System.out.println("Well come to Employee wage computation program");
 	}
+
 	public void monthWage() {
 		int workingHour = 0;
-		int totalWage=0;
-		int totalWorkingHour=0;
-		for (int day = 1; day < maxWorkDay
-	            && totalWorkingHour < maxWorkHr; day++ ) {
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (empCheck) {
-		case 1:
-			workingHour = 8;
-			break;
-		case 2:
-			workingHour = 4;
-			break;
-		default:
+		int totalWage = 0;
+		for (int day = 1; day < maxWorkDay && totalWorkingHour < maxWorkHr; day++) {
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empCheck) {
+			case 1:
+				workingHour = 8;
+				break;
+			case 2:
+				workingHour = 4;
+				break;
+			default:
+			}
+			totalWorkingHour = totalWorkingHour + workingHour;
+			if (totalWorkingHour > maxWorkHr) {
+				totalWorkingHour = maxWorkHr;
+			}
 		}
-		totalWorkingHour = totalWorkingHour + workingHour;
-		int wage = workingHour * wagePerHr;
-		totalWage = totalWage + wage;
-		}
-		System.out.println("Monthly wage of employee: " + totalWage+ "    Total  Working hour is: " +totalWorkingHour);
+
+		totalWage = totalWorkingHour * wagePerHr;
+
+		System.out
+				.println("Monthly wage of employee: " + totalWage + "    Total  Working hour is: " + totalWorkingHour);
 	}
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		EmployeeWageComputation employee = new EmployeeWageComputation();
 		employee.display();
 		employee.monthWage();
